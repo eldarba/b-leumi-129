0147e6e3a60174b1bcd6fe20b44e988c4e1f1c8f
jbt <jbt@404_INS>
2022-09-13 11:52:19 +0300

    micro

---
+++ service-b/src/main/java/app/core/web/ControllerB.java
@@ -0,0 +1,19 @@
+package app.core.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerB {
+	
+	@Value("${spring.cloud.consul.discovery.instanceId}")
+	private String instancId;
+	
+	// http://localhost:????/service/b
+	@GetMapping("/service/b")
+	public String handleA() {
+		return "From: " + instancId;
+	}
+
+}
